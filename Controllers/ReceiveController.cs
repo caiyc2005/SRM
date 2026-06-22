856114047b73412f7cdfaeef334b27b2f1147634
caiyc2005 <2673439720@qq.com>
2026-06-22 21:22:54 +0800

    新增收料控制器

---
+++ Controllers/ReceiveController.cs
@@ -0,0 +1,17 @@
+﻿using backend.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceiveController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<ApiResult>> ReceiveIn()
+        {
+            return Ok();
+        }
+    }
+}
